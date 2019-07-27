eed0bbb8479c25239f744518a7178818f092166d
excsir <excsir@163.com>
2019-07-27 09:29:51 +0800

    1.测试试题文件夹

---
+++ BaseWork/Classes/DataView/ExcSysNotificationName.h
@@ -0,0 +1,18 @@
+//
+//  ExcSysNotificationName.h
+//  Footprint
+//
+//  Created by chao on 2018/8/22.
+//  Copyright © 2018年 chao. All rights reserved.
+//  App 所有自定义的通知
+
+#ifndef ExcSysNotificationName_h
+#define ExcSysNotificationName_h
+
+// 登录登出通知
+#define kUserLoginNotificationName          @"UserLoginNotificationName"
+#define kUserLogoutNotificationName         @"UserLogoutNotificationName"
+
+
+#endif /* ExcSysNotificationName_h */
+
